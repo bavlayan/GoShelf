b849775cd0618e2d82d82ce1723c88adb231efff
bavlayan <b.avlayan@gmail.com>
2022-08-31 23:04:29 +0300

    json key name added

--- internal/data/books.go
+++ internal/data/books.go
@@ -3,16 +3,16 @@ package data
 import "time"
 
 type Book struct {
-	ID          string
-	IsRead      bool
-	Author      string
-	StartedAt   time.Time
-	ReleaseYear int32
-	Quotation   []Quotation
+	ID          string      `json:"id"`
+	IsRead      bool        `json:"is_read"`
+	Author      string      `json:"author"`
+	StartedAt   time.Time   `json:"started_at"`
+	ReleaseYear int32       `json:"release_year"`
+	Quotation   []Quotation `json:"queations"`
 }
 
 type Quotation struct {
-	ID            string
-	BookID        string
-	QuotationNote string
+	ID            string `json:"id"`
+	BookID        string `json:"book_id"`
+	QuotationNote string `json:"quation_note"`
 }
